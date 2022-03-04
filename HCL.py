1076db16fcb0a038ce2475d27b37d8711e8ad0b2
raja2513 <60400994+raja2513@users.noreply.github.com>
2022-03-04 19:08:09 +0530

    Update HCL.py

--- HCL.py
+++ HCL.py
@@ -1,1 +1,10 @@
-print("raja")
+from flask  import *
+app=Flask(__name__)
+@app.route('/s',methods=['POST'])
+def fun():
+  res=request.form
+  f=open('c.csv','a')
+  f.write('\n')
+  s=res.name+','+res.pass
+  f.write(s)
+  f.close()
