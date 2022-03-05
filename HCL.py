a4e4676f8b633752337a42265f81d080ae4ee4a6
raja2513 <60400994+raja2513@users.noreply.github.com>
2022-03-05 08:17:54 +0530

    Update HCL.py

--- HCL.py
+++ HCL.py
@@ -2,9 +2,5 @@ from flask  import *
 app=Flask(__name__)
 @app.route('/s',methods=['POST'])
 def fun():
-  res=request.form
-  f=open('c.csv','a')
-  f.write('\n')
-  s=res.name+','+res.pass
-  f.write(s)
-  f.close()
+   return "raja"
+app.run(debug=True)
